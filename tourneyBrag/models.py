6f706399b2eda560e5f7becb445f1681c28cdf9e
Sean Monroe <seanmonroe86@gmail.com>
2017-04-14 16:12:13 -0400

    Cleaned out models of dead code

--- tourneyBrag/models.py
+++ tourneyBrag/models.py
@@ -3,7 +3,6 @@ from django.db import models
 #If you make changes, make sure to do makemigrations and then migrate.
 #If you add another table, add it to the admin.py so admins can see the table
 
-
 class Player(models.Model):
     username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=30)
@@ -19,7 +18,7 @@ class Player(models.Model):
 
 
 class Organizer(models.Model):
-    username = models.CharField(primary_key=True, max_length=30) # why do we have this
+    username = models.CharField(primary_key=True, max_length=30)
     password = models.CharField(max_length=30)
 
     def __str__(self):
@@ -46,9 +45,6 @@ class Tournament(models.Model):
     tournamentTitle = models.CharField(max_length=30, unique= True, primary_key=True)
     date_created = models.DateTimeField('date_created', auto_now_add=True)
     date_start = models.DateTimeField('date_start')
-
-    #def __str__(self):
-    #   return self.organizerOwner + ", " + str(self.organizerOwnerID) + ", " + self.tournamentTitle + ", " + self.date_created + ", " + self.date_start
 
 
 class Fan(models.Model):
