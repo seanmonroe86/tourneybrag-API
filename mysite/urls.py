0e303df92c99ae5933e9b6994c31a662a8922aab
Sean Monroe <seanmonroe86@gmail.com>
2017-04-15 12:00:22 -0400

    removed unneeded urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,16 +13,10 @@ urlpatterns = [
     url(r'^register', views.Register.as_view()),
     url(r'^become-voucher/', views.VoucherList.as_view()),
     url(r'^ban', views.BanHimList.as_view()),
-    #url(r'^logout', views.Logout.as_view()),
     url(r'^become-fan', views.FanList.as_view()),
     url(r'^application', views.ApplicationList.as_view()),
-    #url(r'^modify-profile', views.ModifyProfile.as_view()),
-    #url(r'^create-tournament', views.CreateTournament.as_view()),
     url(r'^create-match', views.MatchDetail.as_view()),
-    #url(r'^modify-tournament', views.ModifyTournament.as_view()),
-    #url(r'^list-players/', views.PlayerList.as_view()),
     url(r'^list-players', views.PlayersList.as_view()),
-    url(r'^list-organizers', views.OrganizerList.as_view()),
     url(r'^list-tournaments', views.TournamentsList.as_view()),
 ]
 
